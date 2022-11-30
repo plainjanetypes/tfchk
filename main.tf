68af753d0b029f55cc74e5ee41256a2d11f34007
plainjanetypes <plainjanetypes@gmail.com>
2022-11-30 16:25:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  version = "2.4.0"
+  features {}
+}
+resource "azurerm_resource_group" "examtf" {
+  name = "sampletf"
+  location = "East US"
+}
+output "id" {
+  value = data.azurerm_resource_group.examtf.id
+}
