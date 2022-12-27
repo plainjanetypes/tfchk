ddb0d38f026f41391f6927dabe944529b555b196
plainjanetypes <plainjanetypes@gmail.com>
2022-12-27 14:44:26 +0530

    Update sample.tf

--- tfcheckdemo/sample.tf
+++ tfcheckdemo/sample.tf
@@ -1,1 +1,16 @@
+terraform {
+  backend "Azure"
+  {}
+}
 
+provider "azurerm" {
+  version = "2.4.0"
+  features {}
+}
+resource "azurerm_resource_group" "examtf" {
+  name = "sampletf"
+  location = "East US"
+}
+output "id" {
+  value = data.azurerm_resource_group.examtf.id
+}
